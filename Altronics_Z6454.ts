4b72dcae33ffc289f3928c8b033143e559d039a0
AltronicsAUKits <107094635+AltronicsAUKits@users.noreply.github.com>
2022-06-08 14:33:54 +1000

    Initial files for MakeCode project

---
+++ Altronics_Z6454.ts
@@ -0,0 +1,6 @@
+/**
+* This is to indicate all 0the required extension 
+* to su Altroincs Z6454 - Keyestudio KS0426
+* kit are included in this repository
+* Read more at https://www.altronics.com.au/p/z6454-stem-microbit-mini-smart-robot-car-v2.0/
+*/
